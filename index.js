d462354a35469f3e22196ca491b0b0973d7ac64c
Shumei Huang <rose1264@gmail.com>
2017-10-18 19:51:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -59,18 +59,18 @@ function total() {
 
 
 function removeFromCart(item) {
- +  var itemInCart = false
- +  for(var i = 0; i < cart.length; i++){
- +    if (cart[i].hasOwnProperty(item)) {
- +      itemInCart = true;
- +      cart.splice(i, 1);
- +    }
- +  }
- +  if (!itemInCart) {
- +    console.log("That item is not in your cart.");
- +  }
- +  return cart
- +}
+   var itemInCart = false
+   for(var i = 0; i < cart.length; i++){
+         if (cart[i].hasOwnProperty(item)) {
+       itemInCart = true;
+       cart.splice(i, 1);
+     }
+   }
+   if (!itemInCart) {
+     console.log("That item is not in your cart.");
+   }
+   return cart
+ }
 
 function placeOrder(cardNumber) {
   // write your code here
