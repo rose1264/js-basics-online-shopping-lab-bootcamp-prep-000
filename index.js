d20755c9714061d7242a38599ddda36f3ec369b6
Shumei Huang <rose1264@gmail.com>
2017-10-18 19:57:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -59,14 +59,14 @@ function total() {
 
 
 function removeFromCart(item) {
-   var itemInCart = true
+   var itemInCart = false
    for(var i = 0; i < cart.length; i++){
          if (cart[i].hasOwnProperty(item)) {
        itemInCart = true;
        cart.splice(i, 1);
      }
    }
-   if (!itemInCart) {
+   if (itemInCart = false) {
      console.log("That item is not in your cart.");
    }
    return cart
