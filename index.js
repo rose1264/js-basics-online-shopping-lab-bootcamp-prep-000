f24f70dc76ff311e4d2df3f6f1f9515d7fcbd7c4
Shumei Huang <rose1264@gmail.com>
2017-10-18 17:50:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,15 +11,51 @@ function setCart(c) {
 
 function addToCart(item) {
  // write your code here
+ var price = Math.floor(Math.random()*100)+1;
+ var itemObject = {};
+ itemObject[item]=price
+ cart.push(itemObject)
+ console.log(item + " has been added to your cart.");
+ return cart
 }
 
 function viewCart() {
   // write your code here
+  if (cart.length === 0) {
+    console.log("Your shopping cart is empty.");
+  } else if(cart.length < 3){
+    var items = [];
+    for (var i = 0; i < 2; i++) {
+      for (var item in cart[i]) {
+        items.push(item + " at $" + cart[i][item])
+      }
+    }
+    console.log("In your cart, you have " + items.join(" and ") + ".");
+  } else {
+    var items = [];
+    var n = cart.length-1;
+    for (var i = 0; i < n; i++) {
+      for (var item in cart[i]) {
+        items.push(item + " at $" + cart[i][item])
+      }
+    }
+    var lastItem = "cart[n]+' at $' + cart[n][item]";
+    console.log("In your cart, you have " + items.join(", ") + ", and " + lastItem +".");
+  }
+
 }
 
 function total() {
   // write your code here
+  var total=0;
+  for(var i=0;i<cart.lentgh;i++){
+    for (var item in cart[i]){
+      total += cart[i][item];
+    }
+  }
+  return total
 }
+
 
 function removeFromCart(item) {
   // write your code here
